911bed88ebca46739a6bc5e4a0b7b13ee5f31528
mdgtamu <mdg@tamu.edu>
2014-01-30 09:46:27 -0600

    Add PHP includes for header, navbar and footers

---
+++ inc/footer-content.php
@@ -0,0 +1,27 @@
+      <div class="content-footer">
+        <div class="row">
+          <div class="col-sm-6 text-center">
+            <p>Project Name<br>
+            Contact Info Here<br>
+            Contact Line Two<br>
+            <a href="mailto:webmaster@chem.tamu.edu">webmaster@chem.tamu.edu</a>
+            </p>  
+          </div>
+          <div class="col-sm-6 text-center">
+            <p>Copyright &copy; 2013<br>
+            Texas A&amp;M University<br>
+            College Station, TX 77843<br>
+            All Right Reserved
+            </p>  
+          </div>
+          <div class="visible-xs social-footer">
+          	<ul class="text-center">
+              <li><a href="http://www.facebook.com/tamu.chemistry"><img src="assets/ico/social/facebook-footer.png" alt="Chemistry Facebook Page"></a></li>
+              <li><a href="http://twitter.com/TAMUChemistry"><img src="assets/ico/social/twitter-footer.png" alt="Chemistry Twitter Page"></a></li>
+              <li><a href="http://www.linkedin.com/groups/Department-Chemistry-Texas-M-University-3768510"><img src="assets/ico/social/linkedin-footer.png" alt="Chemistry LinkedIn Page"></a></li>  
+            </ul>
+          </div>
+        </div>      
+      </div>
+
+    </div>
---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="assets/ico/favicon.png">
+
+    <title>Department of Chemistry | Texas A&amp;M University</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="assets/css/custom-styles.css" rel="stylesheet">
+    
+    <!-- Custom Fonts -->
+    <!-- For Body -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <!-- For Headings -->
+    <link href='http://fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Header -->
+    <?php include("inc/header.php"); ?>
+    
+    <!-- Static navbar -->
+    <?php include("inc/navbar.php"); ?>
+
+    <div class="container container-main">
+
+      <!-- Main content -->      
+      <div class="row">
+      	<div class="col-lg-12">
+          
+          <h1 class="page-header">Branded Template</h1>
+		  <p>This is an example page from which you can build a site. The above page header is useful for secondary pages (about, contact, etc.), but wouldn't normally be used on the "home" page. There, you may want to employ a "hero" section, or someother marketing content such as a carousel of images.</p>
+          
+          <div class="panel panel-default">
+            <div class="panel-heading">Template Documentation</div>
+              <div class="panel-body">
+                This template is based on version 3.0.0 of the Twitter Bootstrap framework. Please find the Twitter Bootstrap documentation at <a href="http://getbootstrap.com/css">getbootstrap.com/css</a>. Keep in mind that only some of the Bootstrap elements have been customized for this template. If adding more elements, further custumization may be needed.
+              </div>
+          </div>
+
+          
+          <h2>Body Text</h2>
+		  <p>This is body text text. Body fonts should be <a href="http://www.google.com/fonts/specimen/Open+Sans">Google Open Sans</a>, which is already included in this tempalte. Line height should be 1.5. Although there is no specific font size requirement, browser defaults are 16pt so we should keep body text no lower than 14pt (90% to 95% when converted to relative font size for your CSS). Font size might be larger or smaller on particular parts of the page based on their intent.</p>
+          
+ 
+          <div class="row">            
+            <div class="col-sm-6">
+              <p>Let's take a look at what an unordered list will look like:</p>
+              <ul>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+              </ul>
+            </div>
+            <div class="col-sm-6">
+              <p>And a look at what an ordered list loooks like:</p>
+              <ol>
+                <li>First item</li>
+                <li>Second item</li>
+                <li>Third item</li>
+              </ol>
+            </div>
+          </div> <!-- End row -->
+ 
+        </div> <!-- End col-lg-12 -->
+      </div> 
+      <!-- END main content --> 
+      
+      <!-- footer content -->
+      <?php include("inc/footer-content.php"); ?>
+ 
+    
+    <!-- footer page -->
+    <?php include("inc/footer-page.php"); ?>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="assets/js/jquery.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
